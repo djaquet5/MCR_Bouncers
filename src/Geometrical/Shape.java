23605440eb654ce27a2a3b6b853f69028a8a810b
David Jaquet <djaquet5@gmail.com>
2019-02-20 17:05:18 +0100

    Add properties for shape

--- src/Geometrical/Shape.java
+++ src/Geometrical/Shape.java
@@ -2,11 +2,14 @@ package Geometrical;
 
 import java.awt.*;
 
+enum Direction { HORIZONTAL, VERTICAL }
+
 public abstract class Shape {
    private Color color;
    private int size;
    private int posX;
    private int posY;
+   private Direction direction;
 
    public Shape(Color color){
       this.color = color;
