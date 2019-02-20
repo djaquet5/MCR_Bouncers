e0eb97e0738f89deb4752f607f671653f9b9229e
yoann0000 <47739072+yoann0000@users.noreply.github.com>
2019-02-20 17:16:58 +0100

    Update Shape.java

--- src/Geometrical/Shape.java
+++ src/Geometrical/Shape.java
@@ -1,16 +1,36 @@
 package Geometrical;
 
 import java.awt.*;
+import java.util.Random;
+
 
 public abstract class Shape {
-   private Color color;
-   private int size;
+   private final Color color;
+   private final int size;
    private int posX;
    private int posY;
 
    public Shape(Color color){
       this.color = color;
+      Random r = new Random();
+      size = 10 + r.nextInt(40);
+      posX = r.nextInt(500 - size/2) + size/2;
+      posY = r.nextInt(500 - size/2) + size/2;
    }
 
+   public int getPosX() {
+      return posX;
+   }
 
+   public void setPosX(int posX) {
+      this.posX = posX;
+   }
+
+   public int getPosY() {
+      return posY;
+   }
+
+   public void setPosY(int posY) {
+      this.posY = posY;
+   }
 }
