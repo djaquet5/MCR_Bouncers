50b18cb4c8528911316e931b7a12dd21c6aa4d3e
David Jaquet <djaquet5@gmail.com>
2019-02-21 18:06:11 +0100

    Edit the main program

--- src/Bouncers.java
+++ src/Bouncers.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import Geometrical.*;
+import Geometrical.Shape;
 
 public class Bouncers {
 
@@ -9,6 +15,18 @@ public class Bouncers {
       bouncers.setSize(500, 500);
       bouncers.setVisible(true);
 
+      ArrayList<Shape> shapes = new ArrayList<>();
+      for(int i = 0; i < 100; ++i){
+         // Random boolean to create a square or a circle
+         if(getRandomBoolean())
+            shapes.add(new Square(Color.YELLOW));
+         else
+            shapes.add(new Circle(Color.BLUE));
+      }
+   }
 
+   private static boolean getRandomBoolean(){
+      Random random = new Random();
+      return random.nextBoolean();
    }
 }
